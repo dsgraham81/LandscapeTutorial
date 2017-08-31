9fb5ee08d588b4972ffccf20cdd53b468380ec5a
Doug Graham <dsgraham@gmail.com>
2017-08-30 21:52:03 -0500

    First Triangles

--- core/src/com/dsgraham/landscape/Land.java
+++ core/src/com/dsgraham/landscape/Land.java
@@ -1,8 +1,8 @@
 package com.dsgraham.landscape;
 
-import com.badlogic.gdx.graphics.Mesh;
-import com.badlogic.gdx.graphics.VertexAttribute;
-import com.badlogic.gdx.graphics.VertexAttributes;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.*;
+import com.badlogic.gdx.graphics.glutils.ShaderProgram;
 
 /**
  * Created by Doug Graham on 8/30/2017.
@@ -32,21 +32,98 @@ public class Land {
 //    x, y, r, g, b, a,
 //    x, y, r, g, b, a,
 //    ... etc ...
-    protected float[] verts = new float[MAX_VERTS * NUM_COMPONENTS];
+    protected float[] verts;
 
-    //The current index that we are pushing triangles into the array
-    protected int idx = 0;
+    protected short[] indices;
 
     private Mesh mesh;
     private int width;
     private int height;
+    private float[][] heightData;
+    private ShaderProgram shader;
 
 
     public  Land() {
-        mesh = new Mesh(true, MAX_VERTS, 6,
+        shader = new ShaderProgram(Gdx.files.internal("vertex.vert"),
+                Gdx.files.internal("fragment.frag"));
+        width = 4;
+        height = 3;
+
+        mesh = new Mesh(true, width * height * NUM_COMPONENTS, (width -1) * (height -1) * 6,
                 new VertexAttribute(VertexAttributes.Usage.Position, POSITION_COMPONENTS, "a_position"),
                 new VertexAttribute(VertexAttributes.Usage.ColorUnpacked, COLOR_COMPONENTS, "a_color"));
 
+        loadHeightData();
+        setUpVertices();
+        setUpIndices();
+        mesh.setVertices(verts);
+        mesh.setIndices(indices);
+    }
+
+    void setUpVertices(){
+        verts = new float[width * height * NUM_COMPONENTS];
+        int index = 0;
+        for (int y = 0; y < height; y++){
+            for (int x = 0; x < width; x++){
+                verts[index++] = x;
+                verts[index++] = heightData[x][y];
+                verts[index++] = y;
+
+                verts[index++] = 1.0f;
+                verts[index++] = 1.0f;
+                verts[index++] = 1.0f;
+                verts[index++] = 1.0f;
+
+            }
+        }
+    }
+
+    void setUpIndices(){
+        indices = new short[(width -1) * (height -1) * 6];
+        short index = 0;
+        for (int y =0; y < height -1; y++){
+            for (int x = 0; x < width -1; x++){
+                short lowerLeft = (short) (x + y*width);
+                short lowerRight = (short) ((x + 1) + y*width);
+                short topLeft = (short) (x + (y + 1) * width);
+                short topRight = (short) ((x + 1) + (y + 1) * width);
+
+                indices[index++] = lowerLeft;
+                indices[index++] = topLeft;
+                indices[index++] = lowerRight;
+
+                indices[index++] = topLeft;
+                indices[index++] = topRight;
+                indices[index++] = lowerRight;
+            }
+        }
+    }
+
+    void loadHeightData(){
+        heightData = new float[width][height];
+        heightData[0][0] = 0;
+        heightData[1][0] = 0;
+        heightData[2][0] = 0;
+        heightData[3][0] = 0;
+
+        heightData[0][1] = .5f;
+        heightData[1][1] = 0;
+        heightData[2][1] = -1f;
+        heightData[3][1] = .2f;
+
+        heightData[0][2] = 1f;
+        heightData[1][2] = 1.2f;
+        heightData[2][2] = .8f;
+        heightData[3][2] = 0;
+    }
+
+    public void render(Camera cam){
+        shader.begin();
+        shader.setUniformMatrix("u_projTrans", cam.combined);
+
+        mesh.render(shader, GL20.GL_TRIANGLES);
+
+        shader.end();
     }
 
 
--- core/src/com/dsgraham/landscape/LandscapeTutorial.java
+++ core/src/com/dsgraham/landscape/LandscapeTutorial.java
@@ -3,31 +3,34 @@ package com.dsgraham.landscape;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class LandscapeTutorial extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+
+	PerspectiveCamera camera;
+	Land land;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		camera = new PerspectiveCamera(65, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		camera.position.set(0, 10, 0);
+		camera.lookAt(0,0,0);
+		camera.update();
+
+		land = new Land();
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		land.render(camera);
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+
 	}
 }
